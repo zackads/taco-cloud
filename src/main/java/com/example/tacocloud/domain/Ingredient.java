49f3ebae127e69866750d32912b066c755a4a519
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-10-07 12:13:33 +0100

    Define domain classes

---
+++ src/main/java/com/example/tacocloud/domain/Ingredient.java
@@ -0,0 +1,14 @@
+package com.example.tacocloud.domain;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private String id;
+    private String name;
+    private Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
---
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -0,0 +1,8 @@
+package com.example.tacocloud.domain;
+
+import java.util.List;
+
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
