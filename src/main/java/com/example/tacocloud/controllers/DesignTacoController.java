81da14c364e29e4e43ad6a611ab97a2b214a1c74
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-10-20 13:57:13 +0100

    More Bean validation

--- src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
@@ -6,9 +6,11 @@ import com.example.tacocloud.domain.Taco;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +51,7 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco) {
+    public String processTaco(@Valid @ModelAttribute("taco") Taco taco, Errors errors) {
         // Save the taco, to come in Chapter 3
         log.info("Processing taco: " + taco);
 
